9ec38b7551f2c5110a636b2c4114070eaa861f05
Suyash906 <suyash90682711@gmail.com>
2020-10-12 00:24:21 -0700

    extra file removed

--- server/models/counter.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const counterSchema = mongoose.Schema({
-    count: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model('Counter', counterSchema);
